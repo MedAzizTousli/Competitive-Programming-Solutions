40f7cf9ad933b461c1dbe96e862df4e6690b3289
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2019-12-19 04:55:41 +0100

    Update and rename Irreducable Basic Fractions.cpp to 10179 - Irreducable Basic Fractions.cpp

--- OnlineJudge/10179 - Irreducable Basic Fractions.cpp
+++ OnlineJudge/10179 - Irreducable Basic Fractions.cpp
@@ -1,4 +1,4 @@
-// http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1120
+// https://onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1120
 
 #include <iostream>
 
