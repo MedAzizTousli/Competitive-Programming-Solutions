9aabcbd062c352c8b18af98e41c288787fd82dbc
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2019-12-19 05:03:12 +0100

    Update and rename Relatives.cpp to 10299 - Relatives.cpp

--- OnlineJudge/10299 - Relatives.cpp
+++ OnlineJudge/10299 - Relatives.cpp
@@ -1,4 +1,4 @@
-// http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1240
+// https://onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1240
 
 #include <iostream>
 
