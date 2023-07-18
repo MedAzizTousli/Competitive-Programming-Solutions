5971920e6ab2f6ba376fae44b3f910855f7cbf08
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2023-07-18 15:07:36 +0100

    Update TwoSum.py

--- LeetCode/Easy/TwoSum.py
+++ LeetCode/Easy/TwoSum.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(0, len(nums)):
-            # if nums[i] > target:
-            #     continue
             try:
                 j = nums.index(target - nums[i])
             except ValueError:
