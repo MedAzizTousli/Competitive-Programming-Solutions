cc58cd517e4f0b4654b7acb767b0313dfd1947dc
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2023-08-04 11:33:21 +0100

    Create FindNumbersWithEvenNumberOfDigits.py

---
+++ LeetCode/Easy/FindNumbersWithEvenNumberOfDigits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            if len(str(i)) %2 == 0:
+                res += 1
+        return res
