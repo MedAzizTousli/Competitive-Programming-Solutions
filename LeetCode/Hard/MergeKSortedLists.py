a0e340f9e864ffdcb406bcdef8173d138b547b56
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2023-09-24 14:01:34 +0100

    Create MergeKSortedLists.py

---
+++ LeetCode/Hard/MergeKSortedLists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = node = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+
+        node.next = list1 or list2
+
+        return dummy.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+        elif len(lists) == 1:
+            return lists[0]
+        res = self.mergeTwoLists(lists[0], lists[1])
+        for i in range(2, len(lists)):
+            res = self.mergeTwoLists(res, lists[i])
+        return res
