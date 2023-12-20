7cf8102e0285ba4f6f1dcf024df800784d3dd32f
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2023-12-20 22:28:36 +0100

    Create RegularExpressionMatching.py

---
+++ LeetCode/Hard/RegularExpressionMatching.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        cache = [[False] * (len(p) + 1) for i in range(len(s) + 1)]
+        cache[len(s)][len(p)] = True
+
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+                if (j + 1) < len(p) and p[j + 1] == "*":
+                    cache[i][j] = cache[i][j + 2]
+                    if match:
+                        cache[i][j] = cache[i + 1][j] or cache[i][j]
+                elif match:
+                    cache[i][j] = cache[i + 1][j + 1]
+
+        return cache[0][0]
